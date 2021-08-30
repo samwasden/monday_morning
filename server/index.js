fc01e8fa7db01b733434d35d768184780c50eb13
Sam Wasden <samwasden@outlook.com>
2021-08-30 09:34:13 -0600

    fixed errors lol

--- server/index.js
+++ server/index.js
@@ -1,9 +1,11 @@
-import express from "express";
-import path from "path";
+const express = require('express');
+const path = require('path');
 
 const app = express();
 
-app.get('/', path.join(__dirname, '../index.html'))
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../index.html'))
+})
 
 app.use('/css', express.static(path.join(__dirname, '../style.css')));
 
